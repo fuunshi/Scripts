6ed151095acdfa02a99973a1046cb5149d7b9fa5
fuunshi <pranilshrestha2016@gmail.com>
2024-01-29 19:58:44 +0545

    Full Version with audio support

--- Youtube Downloader/README.md
+++ Youtube Downloader/README.md
@@ -8,6 +8,9 @@ A simple automation script that runs through a number of links in 'videos.txt' f
 - yt-dlp
 -> make sure its the latest version.
 
+- ffmpeg
+-> Required for audio formats
+
 ## How to run
 
 Paste youtube links of videos you want to download in each line of videos.txt file
--- Youtube Downloader/script.sh
+++ Youtube Downloader/script.sh
@@ -2,12 +2,32 @@
 
 # Check if the file exists
 if [ ! -f "videos.txt" ]; then
-    echo "File input.txt not found."
+    echo "File videos.txt not found."
     exit 1
 fi
 
-while IFS= read -r link;
-do
-    echo "Working on link : $link"
-    yt-dlp -o "downloads/" $link
-done < "videos.txt"
+echo "Choose an option:"
+echo "1. Download Videos"
+echo "2. Download Audios"
+
+read -p "Enter your choice (1 or 2): " choice
+
+if [ "$choice" == "1" ]; then
+    echo "Downloading Video Format"
+
+    while IFS= read -r link;
+    do
+        echo "Working on link : $link"
+        yt-dlp -o "downloads/%(title)s.%(ext)s" $link
+    done < "videos.txt"
+elif [ "$choice" == "2" ]; then
+    echo "Downloading Audio Format"
+
+    while IFS= read -r link;
+    do
+        echo "Working on link : $link"
+        yt-dlp -x --audio-format mp3 -o "downloads/%(title)s.%(ext)s" $link
+    done < "videos.txt"
+else
+    echo "Invalid choice please enter either 1 or 2"
+fi
